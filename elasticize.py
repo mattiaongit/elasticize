9817440d20aeab72a298403c9713dfc17d448ce4
tia <mattiadmr@gmail.com>
2017-03-24 12:22:08 +0100

    Enabling bulk insert

--- elasticize.py
+++ elasticize.py
@@ -1,17 +1,29 @@
-import requests, pymongo, json, datetime
+import requests
+import pymongo
+import json
+import datetime
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
 config = json.loads(open('config.json', 'r').read())
-
-document_url = '{0}/{1}/{2}/'.format(config['ES_INDEX'],config['ES_INDEX'],config['ES_TYPE'])
 
 connection = pymongo.Connection()
 db = connection[config['MONGO_DATABASE']]
-collection = db[config['MONGO_COLLECTION']].find({}, {'_id': 0})
+es = Elasticsearch(config['ES_URL'])
 
 
 def outputJSON(obj):
     return obj.strftime('%Y-%m-%dT%H:%M:%S')
 
+collection = db[config['MONGO_COLLECTION']].find({})
+documents = []
 
 for n in range(0, collection.count()):
-    document = json.dumps(collection.next(), default=outputJSON)
-    requests.post("{0}{1}".format(document_url, n), data=document)
+    record = collection.next()
+    document = {    "_index": config['ES_INDEX'],
+                    "_type": confing['ES_TYPE'],
+                    "_id": record.id,
+                    "_source": json.dumps(record, default=outputJSON)
+                }
+    documents.append(document)
+
+helpers.bulk(es,documents, stats_only=True, request_timeout= 320)
